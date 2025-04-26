8a00e2abe039509012eb0c4d6b5e8ea8caaf1dab
Promchai Chooseang <promchai2sin@gmail.com>
2025-04-26 18:03:46 +0700

    feat: add worker as parameter for DataModule

--- dataset/lightning.py
+++ dataset/lightning.py
@@ -7,10 +7,11 @@ from dataset.seizure import CHBMITDataset
 
 
 class CHBMITDataModule(L.LightningDataModule):
-    def __init__(self, dataset: CHBMITDataset, batch_size: int = 32):
+    def __init__(self, dataset: CHBMITDataset, batch_size: int = 32, worker=8):
         super().__init__()
         self.dataset = dataset
         self.batch_size = batch_size
+        self.worker = worker
 
     def setup(self, stage: Optional[str] = None) -> None:
         # Split the dataset into train, validation, and test sets
@@ -19,10 +20,12 @@ class CHBMITDataModule(L.LightningDataModule):
         )
 
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=32, shuffle=True, num_workers=10)
+        return DataLoader(
+            self.train_dataset, batch_size=32, shuffle=True, num_workers=self.worker
+        )
 
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=32, shuffle=True, num_workers=10)
+        return DataLoader(self.val_dataset, batch_size=32, num_workers=self.worker)
 
     def test_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=32, shuffle=True, num_workers=10)
+        return DataLoader(self.test_dataset, batch_size=32, num_workers=self.worker)
