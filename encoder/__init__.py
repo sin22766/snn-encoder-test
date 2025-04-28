785b5fd6bac7350d179f6bf6479eb3e0f8d22e56
Promchai Chooseang <promchai2sin@gmail.com>
2025-04-28 16:52:10 +0700

    feat: add Phase Encoder non expand version

--- encoder/__init__.py
+++ encoder/__init__.py
@@ -1,8 +1,8 @@
 from encoder.base import SpikeEncoder
 from encoder.deconv import BSAEncoder
-from encoder.global_refer import PhaseEncoderExpand
+from encoder.global_refer import PhaseEncoder, PhaseEncoderExpand
 from encoder.rate import PoissonEncoder, PoissonEncoderExpand
 from encoder.temporal import StepForwardEncoder, TBREncoder
 
-__all__ = ["SpikeEncoder", "PoissonEncoder", "PoissonEncoderExpand", "StepForwardEncoder", "TBREncoder", "BSAEncoder", "PhaseEncoderExpand"]
+__all__ = ["SpikeEncoder", "PoissonEncoder", "PoissonEncoderExpand", "StepForwardEncoder", "TBREncoder", "BSAEncoder", "PhaseEncoder", "PhaseEncoderExpand"]
 
--- encoder/global_refer.py
+++ encoder/global_refer.py
@@ -3,6 +3,128 @@ import torch
 from encoder.base import SpikeEncoder
 
 
+class PhaseEncoder(SpikeEncoder):
+    """
+    PhaseEncoder for converting continuous values to spike trains using phase encoding.
+
+    This encoder transforms continuous values into a binary representation across multiple 
+    phase windows based on decreasing powers of 2. This version does not expand the time dimension,
+    but downsamples the time dimension by a factor equal to the phase_window parameter.
+
+    The encoded data size may be paded to fit the phase_window size.
+
+    Parameters
+    ----------
+    phase_window : int, optional
+        Number of phase windows to use for encoding. Default is 8.
+    normalize : bool, optional
+        Whether to normalize the input tensor along channel axis. Default is True.
+        
+    Attributes
+    ----------
+    phase_values : torch.Tensor
+        Precomputed threshold values as powers of 2: [2^-1, 2^-2, ..., 2^-phase_window]
+    """
+    def __init__(
+        self,
+        phase_window: int = 8,
+        normalize: bool = True,
+    ):
+        """
+        Initialize the Poisson Encoder.
+
+        Parameters
+        ----------
+        phase_window : int, optional
+            Number of phase windows to use for encoding. Default is 8.
+        normalize : bool, optional
+            Whether to normalize the input tensor along channel axis. Default is True.
+        """
+        super().__init__()
+        self.phase_window = phase_window
+        self.normalize = normalize
+
+        self.phase_values = torch.pow(2.0, -torch.arange(1, phase_window + 1))
+
+    def encode(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Encode a continuous value into a spike train using phase encoding.
+        
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input tensor to be encoded.
+            Expected shape: (batch, channels, freqs, time_steps)
+        
+        Returns
+        -------
+        torch.Tensor
+            Encoded spike train with shape (batch, channels, freqs, time_steps),
+            where the time dimension will be padded to be divisible by phase_window.
+        """
+        if self.normalize:
+            x = self._normalize(x)
+        
+        batch, channels, freqs, time_steps = x.shape
+
+        padding = -time_steps % self.phase_window
+
+        padded_x = torch.nn.functional.pad(x, (0, padding), value=float("nan"))
+
+        mean_x = padded_x.reshape(batch * channels * freqs, -1, self.phase_window).nanmean(dim=-1)
+
+        spike_train = torch.zeros(*mean_x.shape, self.phase_window, dtype=torch.float32,  device=mean_x.device)
+
+        if self.phase_values.device != x.device:
+            self.phase_values = self.phase_values.to(x.device)
+        
+        phase_values = self.phase_values
+
+        for i in range(self.phase_window):
+            mask = mean_x >= phase_values[i]
+            spike_train[:, :, i] = mask.float()
+            mean_x[mask] -= phase_values[i]
+        
+        spike_train = spike_train.reshape(batch, channels, freqs, time_steps + padding)
+        return spike_train
+
+    def decode(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Decode a spike train back to continuous values.
+        
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input spike train to be decoded.
+            Expected shape: (batch, channels, freqs, time_steps)
+        
+        Returns
+        -------
+        torch.Tensor
+            Decoded continuous signal with shape (batch, channels, freqs, time_steps)
+            with values reconstructed from the spike patterns.
+        """
+        batch, channels, freqs, time_steps = x.shape
+
+        time_steps = time_steps // self.phase_window
+
+        x_reshape = x.reshape(batch * channels * freqs, time_steps, self.phase_window)
+
+        if self.phase_values.device != x.device:
+            self.phase_values = self.phase_values.to(x.device)
+        
+        phase_values = self.phase_values
+
+        decoded = x_reshape.matmul(phase_values)
+
+        decoded = decoded.reshape(batch, channels, freqs, time_steps)
+
+        decoded = decoded.repeat_interleave(self.phase_window, dim=-1)
+
+        return decoded
+
+
+
 class PhaseEncoderExpand(SpikeEncoder):
     """
     PhaseEncoderExpand for converting continuous values to spike trains using phase encoding.
@@ -37,6 +159,16 @@ class PhaseEncoderExpand(SpikeEncoder):
         phase_window: int = 8,
         normalize: bool = True,
     ):
+        """
+        Initialize the Poisson Encoder.
+
+        Parameters
+        ----------
+        phase_window : int, optional
+            Number of phase windows to use for encoding. Default is 8.
+        normalize : bool, optional
+            Whether to normalize the input tensor along channel axis. Default is True.
+        """
         super().__init__()
         self.phase_window = phase_window
         self.normalize = normalize
